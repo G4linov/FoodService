52f117df3d4844811ff94af4b8d615e7c6bb71ae
Vyacheslavvvv <galin0ff@yandex.ru>
2024-09-11 10:34:02 +0300

    some upd

--- src/main/java/com/nutries/foodservice/repository/RecipeRepository.java
+++ src/main/java/com/nutries/foodservice/repository/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.nutries.foodservice.repository;
 
 import com.nutries.foodservice.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
